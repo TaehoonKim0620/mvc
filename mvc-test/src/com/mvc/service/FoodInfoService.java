e58fa8431d2aecc7addd68d27c65490ab3f65558
Administrator <Administrator@i7E-13>
2025-02-25 17:49:21 +0900

    e

--- mvc-test/src/com/mvc/service/FoodInfoService.java
+++ mvc-test/src/com/mvc/service/FoodInfoService.java
@@ -6,9 +6,33 @@ import com.mvc.dto.FoodInfoDTO;
 import com.mvc.repository.FoodInfoRepository;
 
 public class FoodInfoService {
+	
+	private FoodInfoRepository fiRepo = new FoodInfoRepository();
+	
 	public List<FoodInfoDTO> getFoodInfos(FoodInfoDTO foodInfo){
-	FoodInfoRepository fiRepo = new FoodInfoRepository();
-	List<FoodInfoDTO> foodInfos = fiRepo.selectFoodInfos(foodInfo);
-	return foodInfos;
+		return fiRepo.selectFoodInfos(foodInfo);
+	}
+	public FoodInfoDTO getFoodInfo(int fiNum) {
+		return fiRepo.selectFoodInfo(fiNum);
+	}
+	public int insertFoodInfo(FoodInfoDTO foodInfo) {
+		return fiRepo.insertFoodInfo(foodInfo);
+	}
+	public int updateFoodInfo(FoodInfoDTO foodInfo) {
+		return fiRepo.updateFoodInfo(foodInfo);
+	}
+	public int deleteFoodInfo(int fiNum) {
+		return fiRepo.deleteFoodInfo(fiNum);
+	}
+	
+	public static void main(String[] args) {
+		FoodInfoService fiService = new FoodInfoService();
+		List<FoodInfoDTO> foodInfos = fiService.getFoodInfos(null);
+		for(FoodInfoDTO foodInfo : foodInfos) {
+			System.out.println(foodInfo);
+		}
+		FoodInfoDTO foodInfo = fiService.getFoodInfo(2);
+		System.out.println(foodInfo);
+		
 	}
 }
